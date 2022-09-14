f70044f7203052b732688dd0d1b928f26b3670b3
naufal zhafari <zhafarinaufal@gmail.com>
2022-09-14 11:13:32 +0700

    Telah Mengcommit File

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    catalog_items = CatalogItem.objects.all()
+    context = {
+        'list_catalog' : catalog_items,
+        'nama' : 'Naufal Zhafari Zahran',
+        'student_id' : '2106752104'
+    }
+    return render(request, 'katalog.html', context)
