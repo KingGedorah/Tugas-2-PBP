5c747c2056058962b1de3675f8dd99f7b2aeeeb1
naufal zhafari <zhafarinaufal@gmail.com>
2022-09-20 15:23:49 +0700

    Telah Menambahkan

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -4,7 +4,7 @@ from mywatchlist.views import show_html, show_json, show_xml
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('mywatchlist/',show_html, name='show_html'),
+    path('mywatchlist/html',show_html, name='show_html'),
     path('mywatchlist/json/',show_json, name='show_json'),
     path('mywatchlist/xml/',show_xml, name='show_xml')
 ]
