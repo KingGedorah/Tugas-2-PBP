9a11e16accb1fed708fad684c2cbe9a589c6e88e
naufal zhafari <zhafarinaufal@gmail.com>
2022-09-21 23:45:20 +0700

    Telah Menambahkan Testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from mywatchlist.views import show_html, show_xml, show_json
+
 
 # Create your tests here.
+class TestingUrl(TestCase):
+    def setUp(self):
+        self.urlViewHtml = reverse('mywatchlist:show_html')
+        self.urlViewJson = reverse('mywatchlist:show_json')
+        self.urlViewXml = reverse('mywatchlist:show_xml')
+    
+    def test_mywatchlist_resolve(self):
+        self.assertEqual(resolve(self.urlViewHtml).func, show_html)
+    
+    def test_mywatchlist_json_resolve(self):
+        self.assertEqual(resolve(self.urlViewJson).func, show_json)
+    
+    def test_mywatchlist_xml_resolve(self):
+        self.assertEqual(resolve(self.urlViewXml).func, show_xml)
