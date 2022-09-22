0681793d7346109f4a04b731e849e7923294e8ca
naufal zhafari <zhafarinaufal@gmail.com>
2022-09-22 10:32:33 +0700

    Telah Menambahkan Fitur

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -7,10 +7,16 @@ from mywatchlist.models import WatchListItem
 def show_html(request):
     watchlist_item = WatchListItem.objects.all()
 
+    watched_film = WatchListItem.objects.filter(status="Watched")
+    notwatched_film = WatchListItem.objects.filter(status="Not Watched")
+
+    watched_status = watched_film.count() >= notwatched_film.count()
+
     context = {
         'list_watchlist' : watchlist_item,
         'nama' : 'Naufal Zhafari Zahran',
-        'student_id' : '2106752104'
+        'student_id' : '2106752104',
+        'check_status' : watched_status,
     }
     return render(request,'mywatchlist.html',context)
 
