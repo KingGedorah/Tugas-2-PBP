b15a93af018443c712f15d461e579fb5db1a8dac
naufal zhafari <zhafarinaufal@gmail.com>
2022-10-09 10:32:36 +0700

    Memperbaiki Kesalahan URL

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -4,7 +4,7 @@ from mywatchlist.views import show_html, show_json, show_xml
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('mywatchlist/html/',show_html, name='show_html'),
-    path('mywatchlist/json/',show_json, name='show_json'),
-    path('mywatchlist/xml/',show_xml, name='show_xml')
+    path('html/',show_html, name='show_html'),
+    path('json/',show_json, name='show_json'),
+    path('xml/',show_xml, name='show_xml')
 ]
